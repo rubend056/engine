a5fa963de81692892303461a695750c616d0a349
RubenD <rubendariopm14@gmail.com>
2020-04-05 19:46:50 -0400

    improving C++ CI

--- deps.sh
+++ deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if hash pacman ; then
-    sudo pacman --needed -S assimp inotify-tools
+    sudo pacman --needed -S glm assimp inotify-tools
 elif hash apt ; then 
-    sudo apt install libglm-dev inotify-tools-dev libinotifytools0-dev 
+    sudo apt install libglm-dev libassimp-dev inotify-tools-dev libinotifytools0-dev 
 fi
