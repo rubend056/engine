ee5bbf34dfdb09a7028892b26a798c17ccb80e1b
RubenD <rubendariopm14@gmail.com>
2020-04-05 22:20:08 -0400

    still fixing code for CI on ubuntu

--- libs/apps/engine/engine.cpp
+++ libs/apps/engine/engine.cpp
@@ -13,6 +13,44 @@
 
 using namespace std;
 
+fs::path relativePath( const fs::path &path, const fs::path &relative_to ){
+    // create absolute paths
+    fs::path p = fs::absolute(path);
+    fs::path r = fs::absolute(relative_to);
+
+    // if root paths are different, return absolute path
+    if( p.root_path() != r.root_path() )
+        return p;
+
+    // initialize relative path
+    fs::path result;
+
+    // find out where the two paths diverge
+    fs::path::const_iterator itr_path = p.begin();
+    fs::path::const_iterator itr_relative_to = r.begin();
+    while( itr_path != p.end() && itr_relative_to != r.end() && *itr_path == *itr_relative_to ) {
+        ++itr_path;
+        ++itr_relative_to;
+    }
+
+    // add "../" for each remaining token in relative_to
+    if( itr_relative_to != r.end() ) {
+        ++itr_relative_to;
+        while( itr_relative_to != r.end() ) {
+            result /= "..";
+            ++itr_relative_to;
+        }
+    }
+
+    // add remaining path
+    while( itr_path != p.end() ) {
+        result /= *itr_path;
+        ++itr_path;
+    }
+
+    return result;
+}
+
 namespace engine{
     bool run = true;
     std::shared_ptr<Scene> scene;
@@ -31,7 +69,8 @@ namespace engine{
 	fs::path get_relative_to_project(const fs::path &asset_path){
 		// Make sure asset path is inside project
 		assert(fs::absolute(asset_path).string().find(project_path.string()) != std::string::npos);
-		return asset_path.lexically_relative(project_path);
+		// return asset_path.lexically_relative(project_path);
+		return relativePath(asset_path, project_path);
 	}
 	void save_scene(){
 		if(scene)
