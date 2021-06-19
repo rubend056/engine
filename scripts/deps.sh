82c4533cf7405e00ba7f391bae82462bc7ec3e4b
Ruben Dario <rubendariopm14@gmail.com>
2021-06-19 04:11:03 -0400

    apt update on deps.sh

--- scripts/deps.sh
+++ scripts/deps.sh
@@ -4,5 +4,6 @@ set -e
 if hash pacman;then
 		sudo pacman --noconfirm --needed -S glm assimp inotify-tools
 elif hash apt;then
+		sudo apt update
 		sudo apt-get -y install cmake build-essential libsdl2-dev libglm-dev libassimp-dev libinotifytools0-dev libx11-dev
 fi;
